0b8fb3bae33411b8161f5e72708425ff9d1833df
Brian Moniz <bmoniz328@gmail.com>
2022-01-12 15:00:13 -0800

    fundamentals 1

---
+++ FundamentalsToDo1/script.js
@@ -0,0 +1,95 @@
+// Setting and Swapping
+
+// let myNumber = 42;
+// let myName = "Brian"
+
+// console.log(myNumber, myName)
+
+// myNumber = myName
+// myName = 42
+
+// console.log(myNumber, myName)
+
+// Print -52 to 1066
+
+// for (let i = -52; i <= 1066; i++){
+//     console.log(i)
+// }
+
+// Don't worry, be happy
+
+// const beCheerful = () => {
+//     for (let i = 1; i <= 98; i++) {
+//         console.log("good morning ", i)
+//     }
+// }
+// beCheerful();
+
+// Multiples of Three - but not all
+
+// for (let i = 3; i >= -300; i--) {
+
+//         if (i === -3 || i === -6) {
+//             continue
+//         }
+//     console.log(i)
+// }
+
+// Printing Integers with While
+
+// let number = 2000;
+
+// while (number <= 5280){
+//     console.log(number)
+//     number++
+// }
+// console.log("Finished")
+
+// You say it's your birthday
+// not sure what this problem is even asking us to do here????
+
+// if (num1 === 3 || num2 === 28) {
+//     console.log("How did you know?")
+// } else {
+//     console.log("Just another day....")
+// }
+
+// Leap Year
+
+// function isItLeapYear(year) {
+//     if (year % 4 == 0 && year % 100 != 0){
+//         console.log("It is a leap year")
+//     } else if (year % 100 == 0) {
+//         console.log("It's not a leap year")
+//     } else if (year % 400 == 0) {
+//         console.log("This is a leap year")
+//     } else {
+//         console.log("Not a leap year")
+//     }
+// }
+
+// isItLeapYear(2022)
+
+// Print and Count
+// let count = 0;
+
+// for (let i = 512; i <= 4096; i++) {
+//     if (i % 5 == 0){
+//         console.log(i)
+//         count++;
+//     }
+// }
+// console.log(count)
+
+// Multiples of 6
+
+var num = 6
+
+while (num <= 60000)
+{
+    if (num % 6 == 0) {
+        console.log(num)
+    }
+    num++
+}
+console.log("Finished")
