44e1fbbe2fd55040b64ee86cf81ff19c4ade7be5
Brian Moniz <bmoniz328@gmail.com>
2022-01-16 20:00:49 -0800

    test case

--- ArraysToDo5/script.js
+++ ArraysToDo5/script.js
@@ -4,6 +4,9 @@
 
 function arrayAverage(array){
     let sum = 0
+    if (array[0] === null){
+        return 0;
+    }
     for (let i = 0; i < array.length; i++){
         sum = array[i] + sum;
     }
@@ -11,3 +14,4 @@ function arrayAverage(array){
 }
 
 console.log("Array Averages: ", arrayAverage([1, 2, 3, 12, 6, 11, 5, 4, 0, 10, 7, 8, 9]))
+console.log("averages: ", arrayAverage([]))
